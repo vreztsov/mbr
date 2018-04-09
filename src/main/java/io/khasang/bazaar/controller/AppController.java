bb1222902930e7a9a4f07fdecedf5f14d506a98b
Viktor <vreztsov90@mail.ru>
2018-04-09 20:54:36 +0300

    added my abrakadabra

--- src/main/java/io/khasang/bazaar/controller/AppController.java
+++ src/main/java/io/khasang/bazaar/controller/AppController.java
@@ -6,5 +6,11 @@ public class AppController {
         System.out.println("56e5ywtydag");
         System.out.println("s5ywe5uisayw24dag");
         System.out.println("sd24365w4e57ag");
+        int i=0;
+        i++;
+        while (true){
+            i+=5;
+            if (i>324) break;
+        }
     }
 }
