482072c12f733b25a4c8de07ceed838cc25bf49f
Viktor <vreztsov90@mail.ru>
2018-04-09 20:58:19 +0300

    added trash

--- src/main/java/io/khasang/bazaar/controller/AppController.java
+++ src/main/java/io/khasang/bazaar/controller/AppController.java
@@ -2,9 +2,8 @@ package io.khasang.bazaar.controller;
 
 public class AppController {
     public static void main(String[] args) {
-        System.out.println("3445ywe5g");
-        System.out.println("56e5ywtydag");
-        System.out.println("s5ywe5uisayw24dag");
-        System.out.println("sd24365w4e57ag");
+        for (int i = 0; i < 444; i++) {
+            System.out.println("Хэлло Ворлд! Лэт'c вотч ворлд вэдыр");
+        }
     }
 }
