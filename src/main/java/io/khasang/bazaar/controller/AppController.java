09701010c7ec14662178c85ce3917dcadb9b6a93
Viktor <vreztsov90@mail.ru>
2018-04-09 20:51:24 +0300

    added my abrakadabra

--- src/main/java/io/khasang/bazaar/controller/AppController.java
+++ src/main/java/io/khasang/bazaar/controller/AppController.java
@@ -2,9 +2,9 @@ package io.khasang.bazaar.controller;
 
 public class AppController {
     public static void main(String[] args) {
-        System.out.println("sdgssdag");
-        System.out.println("sdgssdag");
-        System.out.println("sdgssdag");
-        System.out.println("sdgssdag");
+        System.out.println("3445ywe5g");
+        System.out.println("56e5ywtydag");
+        System.out.println("s5ywe5uisayw24dag");
+        System.out.println("sd24365w4e57ag");
     }
 }
